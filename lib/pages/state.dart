12abc64399991882c91b76d05afbecfc543047a4
JVAVmusic <kyleedwardgjava@gmail.com>
2020-09-08 22:51:40 -0700

    Initial Commit

---
+++ lib/pages/state.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+//This file displays the covid cases for a single state
+
+class DisplayState extends StatefulWidget {
+  @override
+  _DisplayStateState createState() => _DisplayStateState();
+}
+
+class _DisplayStateState extends State<DisplayState> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/stateslist.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+//This file shows a list of states in the US that the user can choose to view
+
+class ListOfStates extends StatefulWidget {
+  @override
+  _ListOfStatesState createState() => _ListOfStatesState();
+}
+
+class _ListOfStatesState extends State<ListOfStates> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
